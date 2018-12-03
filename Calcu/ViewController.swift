2b07b98c74c05e29da98e11bd414731d0abb5bc2
Tyrone Dizon <tyroned3@109c02vx09mj1gc.student.cssd.ab.ca>
2018-12-03 14:23:13 -0700

    Initial Commit

---
+++ Calcu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calcu
+//
+//  Created by Tyrone Dizon on 2018-12-03.
+//  Copyright © 2018 Tyrone Dizon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
